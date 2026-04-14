a2f3d880771cb207f4ac1da1e8654a344efaf9b1
asadezidi999-oss <asadezidi999@gmail.com>
2026-04-15 00:24:48 +0200

    Update service-worker.js

--- challenge/service-worker.js
+++ challenge/service-worker.js
@@ -1,4 +1,30 @@
-{
+const CACHE_NAME = "work-tracker-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});{
   "name": "Challenge App",
   "short_name": "Challenge",
   "start_url": "index.html",
