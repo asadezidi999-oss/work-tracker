30408884eb1a58a860ca9dfd5345d0cd86d473c2
asadezidi999-oss <asadezidi999@gmail.com>
2026-04-16 13:12:04 +0200

    Update service-worker.js

--- deutsch-app/service-worker.js
+++ deutsch-app/service-worker.js
@@ -4,20 +4,39 @@ const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./android-chrome-192x192.png",
-  "./android-chrome-512x512.png"
+  "./192x192.png",
+  "./512x512.png"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// ACTIVATE (تنظيف الكاش القديم)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (offline support)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
