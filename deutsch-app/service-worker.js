7559513d3a6cb1fecbb124923f1cac3a27d1dee2
asadezidi999-oss <asadezidi999@gmail.com>
2026-04-16 12:27:56 +0200

    Update service-worker.js

--- deutsch-app/service-worker.js
+++ deutsch-app/service-worker.js
@@ -1,1 +1,15 @@
+const CACHE_NAME = "app-v1";
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(["./", "./index.html"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
