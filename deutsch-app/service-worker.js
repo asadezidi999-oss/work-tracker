157e15435ba6b958c2cc85b6e196e3c641857f3a
asadezidi999-oss <asadezidi999@gmail.com>
2026-04-20 19:30:04 +0200

    Refactor service worker to use ASSETS array

--- deutsch-app/service-worker.js
+++ deutsch-app/service-worker.js
@@ -1,24 +1,35 @@
 const CACHE_NAME = "deutsch-app-v1";
 
-const urlsToCache = [
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./assets/icon.png"
+  "./assets/android-chrome-192x192.png",
+  "./assets/android-chrome-512x512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
